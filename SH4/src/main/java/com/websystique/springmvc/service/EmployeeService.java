0015cf037cb04a2eeb89c37901ea8e4507ad0da4
yoboygo <yoboygo@gmail.com>
2017-02-28 16:30:12 +0800

    整合Spring4 + Hibernate4 Demo 全部注解实现

---
+++ SH4/src/main/java/com/websystique/springmvc/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.websystique.springmvc.service;
+
+import java.util.List;
+
+import com.websystique.springmvc.model.Employee;
+
+public interface EmployeeService {
+
+	Employee findById(int id);
+	void saveEmpliyee(Employee employee);
+	void updateEmployee(Employee employee);
+	void deleteEmployeeBySsn(String ssn);
+	List<Employee> findAllEmployees();
+	Employee findEmployeeBySsn(String ssn);
+	boolean isEmployeeSsnUnique(Integer id,String ssn);
+}
---
+++ SH4/src/main/java/com/websystique/springmvc/service/EmployeeServiceImpl.java
@@ -0,0 +1,61 @@
+package com.websystique.springmvc.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.websystique.springmvc.dao.EmployeeDao;
+import com.websystique.springmvc.model.Employee;
+@Service("employeeService")
+@Transactional
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeDao dao;
+	
+	public Employee findById(int id) {
+		return dao.findById(id);
+	}
+
+	public void saveEmpliyee(Employee employee) {
+		dao.saveEmployee(employee);
+
+	}
+	
+	/*
+     * Since the method is running with Transaction, No need to call hibernate update explicitly.
+     * Just fetch the entity from db and update it with proper values within transaction.
+     * It will be updated in db once transaction ends. 
+     */
+	public void updateEmployee(Employee employee) {
+		Employee entity = dao.findById(employee.getId());
+		if(entity != null){
+			entity.setJoiningDate(employee.getJoiningDate());
+			entity.setName(employee.getName());
+			entity.setSalary(employee.getSalary());
+			entity.setSsn(employee.getSsn());
+		}
+	}
+
+	public void deleteEmployeeBySsn(String ssn) {
+		dao.deleteEmployeeBySsn(ssn);
+
+	}
+
+	public Employee findEmployeeBySsn(String ssn) {
+		return dao.findEmployeeBySsn(ssn);
+	}
+
+	public boolean isEmployeeSsnUnique(Integer id, String ssn) {
+		Employee employee = findEmployeeBySsn(ssn);
+		return (employee == null || ((id != null) && (employee.getId() == id)));
+	}
+
+	public List<Employee> findAllEmployees() {
+		return dao.findAllEmployees();
+	}
+
+}
